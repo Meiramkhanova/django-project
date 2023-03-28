d1a0c5479993beb9ece652b7cada636ac96f194a
User <adina.meiramkhanova@narxoz.kz>
2023-03-28 10:24:57 +0600

    added forms

--- coolsite/people/forms.py
+++ coolsite/people/forms.py
@@ -3,7 +3,7 @@ from .models import *
 
 class AddPostForm(forms.Form):
     name = forms.CharField(max_length=255, label="Name")
-    age =  forms.CharField(max_length=255, label="Age")
+    age = forms.CharField(max_length=255, label="Age")
     country = forms.CharField(max_length=255, label="Country")
     info_content = forms.CharField(widget=forms.Textarea(attrs={'cols': 60, 'rows': 10}), label="Content")
-    is_published = forms.BooleanField(label="Publication")
+    is_published = forms.BooleanField()
