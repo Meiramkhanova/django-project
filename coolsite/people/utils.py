7f6592798cd24de040565a4a82d7ea9f07e162c4
User <adina.meiramkhanova@narxoz.kz>
2023-04-13 17:32:32 +0600

    Added caching

--- coolsite/people/utils.py
+++ coolsite/people/utils.py
@@ -1,4 +1,5 @@
 from django.db.models import Count
+from django.core.cache import cache
 
 from .models import *
 
@@ -12,7 +13,10 @@ class DataMixin:
     paginate_by = 3
     def get_user_context(self, **kwargs):
         context = kwargs
-        cats = Category.objects.annotate(Count('people'))
+        cats  = cache.get('cats')
+        if not cats:
+            cats = Category.objects.annotate(Count('people'))
+            cache.set('cats', cats, 60)
 
         user_menu = menu.copy()
         if not self.request.user.is_authenticated:
